0d9878fa0571aa231274a01ecdb2c961a53e5e87
Zach <Lavakz98@uwosh.edu>
2023-06-04 06:41:08 -0500

    songNames

--- src/app/music/audio-player/audio-player.component.ts
+++ src/app/music/audio-player/audio-player.component.ts
@@ -4,48 +4,99 @@ import { CommonModule } from '@angular/common';
 //@ts-ignore
 import WaveSurfer from 'wavesurfer.js';
 
+let musicDir: string = '../assets/Audio/myMusic/';
+function getSongNames(): string[] {
+  // Todo: Get these from SQL database
+  return [
+    'amoeba',
+    'Cyclomachine',
+    'Darkwraith',
+    'Daze',
+    'Deep',
+    'Focus',
+    'Jumble',
+    'LacunaLiner',
+    'LonelyHarp',
+    'Magnapinna',
+    'Protozoa',
+    'Remember',
+    'Spirillum',
+    'Towtacular',
+  ];
+}
+let musicPaths: string[] = [];
+
+function getMusicPaths(refresh: boolean = false): string[] {
+  if (!refresh && musicPaths.length > 0) return musicPaths;
+  musicPaths = [];
+  let songNames = getSongNames()
+  songNames.forEach((songName) => {
+    musicPaths.push(musicDir.concat(songName).concat('.mp3'));
+  });
+  return musicPaths;
+}
+
 @Component({
   selector: 'app-audio-player',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div id="audioPlayer">
-      <button id="playButton">Play</button>
-      <div id="waveform"></div>
+    <div id="controlPanel">
+      <div id="songWindow"><h1 id="songTitle">{{ songNames[songIndex] }}</h1></div>
+      <button type="button" class="navigation" (click)="incrementSongIndex()">^</button>
+      <div id="audioPlayer">
+        <button id="playButton">Play</button>
+        <div id="waveform"></div>
+      </div>
+      <button type="button" class="navigation" (click)="incrementSongIndex(true)">v</button>
     </div>
   `,
   styleUrls: ['./audio-player.component.sass'],
 })
 export class AudioPlayerComponent {
   public static loadedSongs: Song[] = [];
-
+  songNames: string[] = []
+  songIndex: number = 0;
   constructor() {
+    this.songNames = getSongNames();
     window.addEventListener('resize', () => {
-      AudioPlayerComponent.loadedSongs.forEach((wave) => {
-        wave.destroy();
-      });
-      AudioPlayerComponent.loadedSongs = [];
-      loadWave();
+      this.refreshSong()
     });
   }
   ngOnInit() {
-    loadWave();
+    loadSong();
   }
-
-
+  incrementSongIndex(down: boolean = false): void {
+    this.songIndex = this.songIndex + (down ? -1 : 1);
+    this.refreshSong()
+  }
+  refreshSong() {
+    this.songNames = getSongNames();
+    AudioPlayerComponent.loadedSongs.forEach((wave) => wave.destroy());
+    loadSong();
+  }
 }
 
-function loadWave() {
-  const song = new Song();
+function loadSong(
+  index: number = Math.floor(Math.random() * getMusicPaths().length)
+): Song {
+  AudioPlayerComponent.loadedSongs = [];
+  let song = new Song(getMusicPaths()[index]);
   AudioPlayerComponent.loadedSongs.push(song);
-  return song
+  return song;
 }
 
 class Song {
   private wavesurfer: WaveSurfer;
   public isPlaying: boolean = false;
 
-  constructor(containerSeletor: string = '#waveform') {
+  constructor(songPath: string, containerSeletor: string = '#waveform') {
+    this.createWaveSurfer(containerSeletor);
+    this.wavesurfer.load(songPath);
+    this.addPlayButton();
+  }
+
+  private createWaveSurfer(containerSeletor: string | null) {
     this.wavesurfer = WaveSurfer.create({
       container: containerSeletor,
       waveColor: 'violet',
@@ -53,21 +104,16 @@ class Song {
       height: 80,
       barWidth: 2,
     });
-    this.wavesurfer.load('../assets/Audio/myMusic/Daze.mp3');
-    const playButton = document.getElementById('playButton');
-    if (playButton) {
-      playButton.addEventListener('click', () => {
-        this.wavesurfer.playPause();
-        this.isPlaying = !this.isPlaying;
-        if (this.isPlaying) {
-          playButton.innerHTML = 'Pause';
-        } else {
-          playButton.innerHTML = 'Play';
-        }
-      });
-    } else {
-      console.log('playButton not found');
-    }
+  }
+
+  private addPlayButton(buttonId: string = 'playButton') {
+    const playButton = document.getElementById(buttonId);
+    if (!playButton) return;
+    playButton.addEventListener('click', () => {
+      this.wavesurfer.playPause();
+      this.isPlaying = !this.isPlaying;
+      playButton.innerHTML = this.isPlaying ? 'Pause' : 'Play';
+    });
   }
 
   public destroy(): void {
